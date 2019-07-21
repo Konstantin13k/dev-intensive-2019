b09ba99fd9f7060fdc5109eda910605f344ab6fd
koks13 <kostyak13k@gmail.com>
2019-07-21 15:52:25 +0300

    hometask_3 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,10 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard() {
+    val inputMethodManager: InputMethodManager =
+        this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(this.currentFocus.windowToken, 0)
+}
